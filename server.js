f60c700fed97a52de8ccad6df6221be6789d662b
Your Name <you@example.com>
2024-07-06 15:43:21 -0300

    falta

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 import express from "express";
+import PreguntasController from "./controller/preguntas-controller";
+import RespuestaController from "./controller/respuesta-controller";
 
+app.use(express.json());
+app.use("/api/preguntas", PreguntasController);
+app.use("/api/respuestas", RespuestaController);
 
 const app = express();
 app.use(express.json());
